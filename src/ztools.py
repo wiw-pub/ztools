ef475283685c18529e81f2c795d57cd5ae6278dc
wiw <wiw@wiw-dsk>
2025-12-28 02:44:06 -0800

    Swap to use monads from main branch now.

--- src/ztools.py
+++ src/ztools.py
@@ -5,8 +5,7 @@ from collections.abc import Iterable
 
 from transformlineagemonad import TransformLineageMonad
 
-#nimport('https://raw.githubusercontent.com/wiw-pub/ztools/refs/heads/main/src/transformlineagemonad.py')
-nimport('https://raw.githubusercontent.com/wiw-pub/ztools/refs/heads/monads/src/transformlineagemonad.py')
+nimport('https://raw.githubusercontent.com/wiw-pub/ztools/refs/heads/main/src/transformlineagemonad.py')
 
 '''
 WIP: generalized set of operators for higher-level abstraction for pythonscad.
